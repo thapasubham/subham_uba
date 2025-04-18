1e9dce7673f8273941c96cf35bf8f31321372b67
Subham Thapa <subhamits4.2@gmail.com>
2025-04-18 12:39:56 +0545

    develop

--- src/types/UserController.ts
+++
@@ -1,7 +0,0 @@
-import {user } from './user.type.js'
-export abstract class User{
-    abstract CreateUser(user: user):void;
-    abstract DeleteUser(user:user):void;
-    abstract ReadUser(): void;
-    abstract Update(oldUser :user, user: user): void;
-}
--- src/types/user.type.ts
+++
@@ -1,4 +0,0 @@
-export type user ={
-    firstname: string,
-    lastname?:string,
-}
