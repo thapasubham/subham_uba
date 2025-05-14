423aa3c68e709f93bef1037ff01c2ccac9bf1aef
Subham Thapa <subhamits4.2@gmail.com>
2025-05-14 16:18:17 +0545

    refactored to use DB_PORT instead of PORT

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,7 +5,7 @@ import { user } from "./entity/user.js";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: Number(process.env.PORT),
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
