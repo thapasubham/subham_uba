cf74e161cc2a5cb1a9e4bfa191273fc0c831d15e
Subham Thapa <subhamits4.2@gmail.com>
2025-05-15 15:08:12 +0545

    added the entity in the DataSource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Intern, user } from "./entity/user.js";
+import { Intern, Mentors, user } from "./entity/user.js";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
-  entities: [user, Intern],
+  entities: [user, Intern, Mentors],
   migrations: ["migration/*.js"],
   subscribers: [],
 });
